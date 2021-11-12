ab1f088bf4d302e4e6c601eca2e60c1adab185ec
FloMendoza <2003035@ub.edu.ph>
2021-11-12 21:43:24 +0800

    1stpart

---
+++ Activity1_Mendoza.py
@@ -0,0 +1,7 @@
+prelims = int(input('Enter Prelims Grade:'))
+midterms = int(input('Enter Midterm Grade:'))
+semifinals = int(input('Enter Semi-Finals Grade:'))
+finals = int(input('Enter Finals Grade:'))
+
+average = (prelims + midterms + semifinals + finals)/4
+
