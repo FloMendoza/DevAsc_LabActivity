43e53becf808ec8659beda522c7ebcd104cee304
FloMendoza <2003035@ub.edu.ph>
2021-11-12 22:29:16 +0800

    3rdpart

--- Activity1_Mendoza.py
+++ Activity1_Mendoza.py
@@ -5,7 +5,16 @@ finals = int(input('Enter Finals Grade:'))
 
 average = (prelims + midterms + semifinals + finals)/4
 
-if average>=75:
-    print("Passed") 
-else:
-    print("Failed")
+
+if (average < 60):
+    print("Your grade is E")
+elif(average >= 61 or average <= 70):
+    print("Your grade is E")
+elif(average >= 71 or average <= 79):
+    print("Your grade is D")
+elif(average >= 80 or average <= 89):
+    print("Your grade is C")
+elif(average >= 90 or average <= 98):
+    print("Your grade is B")
+elif(average > 99):
+    print("Your grade is A")
